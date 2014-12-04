14ed2dbedb192b7dd78ebbc9b5b0a85d2e419f82
Koushik Pal <koushik.pal@gmail.com>
2014-12-04 02:49:59 -0600

    minor corrections

--- plot2.R
+++ plot2.R
@@ -1,4 +1,11 @@
-attach(subsetData)
+data <- read.table("household_power_consumption.txt", header=T, sep=";", 
+                   na.strings="?", nrows=1000000, stringsAsFactors=F)
+data$DateTime <- paste(data$Date, data$Time)
+data$DateTime <- as.Date(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
+
+library(dplyr)
+subsetData <- filter(data, DateTime >= as.Date("2007-02-01 00:00:00"), 
+                     DateTime < as.Date("2007-02-03 00:00:00"))
 
 png("plot2.png",width=400, height=500)
 plot(subsetData$Global_active_power, type="l", xaxt='n', 
--- plot4.R
+++ plot4.R
@@ -1,6 +1,13 @@
-attach(subsetData)
+data <- read.table("household_power_consumption.txt", header=T, sep=";", 
+                   na.strings="?", nrows=1000000, stringsAsFactors=F)
+data$DateTime <- paste(data$Date, data$Time)
+data$DateTime <- as.Date(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
 
-png("plot4.png",width=500, height=500)
+library(dplyr)
+subsetData <- filter(data, DateTime >= as.Date("2007-02-01 00:00:00"), 
+                     DateTime < as.Date("2007-02-03 00:00:00"))
+
+png("plot4.png", width=500, height=500)
 
 par(mfrow = c(2,2))
 
